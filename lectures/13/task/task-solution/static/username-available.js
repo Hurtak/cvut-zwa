f60e5a637284be9abb1b0cd8b8d99c254045aad1
Petr Huřťák <petr.hurtak@gmail.com>
2020-01-08 09:31:42 +0100

    13: update

--- lectures/13/task/task-solution/static/username-available.js
+++ lectures/13/task/task-solution/static/username-available.js
@@ -1,39 +1,48 @@
-var inputEl = document.querySelector(".js-username");
+var inputEl = document.querySelector(".js-username-input");
 var errorEl = document.querySelector(".js-username-error");
 var request = null;
 
 inputEl.addEventListener("input", function(event) {
-  setErrorVisibility(false);
-
-  if (request) {
-    request.abort();
-  }
-
-  request = new window.XMLHttpRequest();
-  request.addEventListener("load", function(event) {
-    if (event.target.status === 200) {
-      var res = JSON.parse(event.target.responseText);
-      var usernameAvailable = res.usernameAvailable;
-      setErrorVisibility(!usernameAvailable);
-    } else {
-      setErrorVisibility(false);
-    }
-  });
-  request.addEventListener("error", function(error) {
     setErrorVisibility(false);
-  });
 
-  var username = event.target.value;
-  request.open("GET", "./api/username-available.php?username=" + encodeURIComponent(username));
-  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  request.setRequestHeader("Accept", "application/json");
-  request.send();
+    if (request) {
+        request.abort();
+    }
+
+    request = new window.XMLHttpRequest();
+    request.addEventListener("load", function(event) {
+        if (event.target.status === 200) {
+            var res
+            try {
+                var res = JSON.parse(event.target.responseText);
+            } catch (e) {
+                console.log('Error parsing response', e);
+                return;
+            }
+
+            var usernameAvailable = res.usernameAvailable;
+            setErrorVisibility(!usernameAvailable);
+        } else {
+            console.log("Non 200 status code", event.target);
+            setErrorVisibility(false);
+        }
+    });
+    request.addEventListener("error", function(error) {
+        console.log("Error getting the data", error);
+        setErrorVisibility(false);
+    });
+
+    var username = event.target.value;
+    request.open("GET", "./api/username-available.php?username=" + encodeURIComponent(username));
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.setRequestHeader("Accept", "application/json");
+    request.send();
 });
 
 function setErrorVisibility(visible) {
-  if (visible) {
-    errorEl.hidden = false;
-  } else {
-    errorEl.hidden = true;
-  }
+    if (visible) {
+        errorEl.hidden = false;
+    } else {
+        errorEl.hidden = true;
+    }
 }
